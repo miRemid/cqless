3133e5d1555c68f48f625e0ed72780b6046ed3e4
miRemid <miosuki241@outlook.com>
2023-05-05 22:42:35 +0800

    feat: 添加函数代理限流功能
    防止过多请求代理到函数容器，使用官方限流器`time/rate`进行限流

---
+++ pkg/middleware/rateLimit.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/miRemid/cqless/pkg/httputil"
+	"github.com/miRemid/cqless/pkg/types"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/time/rate"
+)
+
+func RateLimit(rateConfig *types.RateLimitConfig) gin.HandlerFunc {
+	log.Info().Float64("rateLimit.limit", rateConfig.Limit).Int("rateLimit.Burst", rateConfig.Burst).Msg("已启动令牌桶功能")
+	limit := rate.NewLimiter(rate.Limit(rateConfig.Limit), rateConfig.Burst)
+	return func(ctx *gin.Context) {
+		if limit.Allow() {
+			ctx.Next()
+		} else {
+			log.Debug().Msg("过多请求，拒绝调用")
+			ctx.AbortWithStatusJSON(http.StatusTooManyRequests, httputil.Response{})
+		}
+	}
+}
